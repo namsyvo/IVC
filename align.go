fe0dc2d2a728b48b5ff5e9e3688c9b84251a2cda
Nam S Vo <nsvo@delphinus.cs.memphis.edu>
2014-07-17 00:25:02 -0500

    Memory efficient version

--- align.go
+++ align.go
@@ -18,11 +18,10 @@ import (
     "github.com/vtphan/fmi" //to use FM index
 )
 
-//Global variables
 var (
-    DIST_THRES int = math.MaxInt16 //threshold for distances between reads and multigenomes
-    ITER_NUM int //number of random iterations to find proper seeds
-    MAXIMUM_MATCH int //maximum number of matches
+    DIST_THRES int = INF //threshold for distances between reads and multigenomes
+    ITER_NUM int = INF //number of random iterations to find proper seeds
+    MAXIMUM_MATCH int = INF //maximum number of matches
 )
 
 //--------------------------------------------------------------------------------------------------
@@ -49,21 +48,19 @@ func (I *Index) Init(genome_file, snp_file, index_file, rev_index_file string, r
     fmt.Println("DIST_THRES: ", DIST_THRES)
     fmt.Println("ITER_NUM: ", ITER_NUM)
     fmt.Println("MAXIMUM_MATCH: ", MAXIMUM_MATCH)
-    fmt.Println("INF_VALUE: ", INF)
 }
 
 //--------------------------------------------------------------------------------------------------
 // Bachward Search with FM-index, start from any position on the pattern.
 //--------------------------------------------------------------------------------------------------
-
-func (I Index) BackwardSearchFrom(index fmi.Index, pattern []byte, start_pos int) []int {
+func (I *Index) BackwardSearchFrom(index fmi.Index, pattern []byte, start_pos int) (int, int, int){
 	var sp, ep, offset int
 	var ok bool
 
 	c := pattern[start_pos]
 	sp, ok = index.C[c]
-	if ! ok {
-		return make([]int, 0)
+	if !ok {
+		return 0, -1, -1
 	}
 	ep = index.EP[c]
 	var sp0, ep0 int
@@ -79,57 +76,50 @@ func (I Index) BackwardSearchFrom(index fmi.Index, pattern []byte, start_pos int
 				sp = sp0
 				ep = ep0
 			} else {
-				return []int{sp, ep, i + 1}
+				return sp, ep, i + 1
 			}
 		} else {
-			return make([]int, 0)
+			return  0, -1, -1
 		}
   		// if Debug { fmt.Println("\t", string(c), sp, ep) }
 	}
- 	return []int{sp, ep, 0}
+	return sp, ep, 0
 }
 
 //--------------------------------------------------------------------------------------------------
 // FindSeeds function returns positions and distances of LCS between reads and multi-genomes.
 // It uses both backward search and forward search (backward search on reverse references).
 //--------------------------------------------------------------------------------------------------
-func (I Index) FindSeeds(read []byte, p int) (int, int, []int, bool) {
-    read_len := len(read)
-    rev_read := make([]byte, read_len)
-    for i := 0; i < read_len; i++ {
-        rev_read[i] = read[read_len - i - 1]
-    }
+func (I *Index) FindSeeds(read, rev_read []byte, p int, match_pos []int) (int, int, int, bool) {
+
     var rev_sp, rev_ep int = 0, MAXIMUM_MATCH
     var rev_s_pos, rev_e_pos, s_pos, e_pos int
-    var rev_result []int
 	
-    rev_s_pos = read_len - 1 - p
-    rev_result = I.BackwardSearchFrom(I.REV_FMI, rev_read, rev_s_pos)
-    rev_sp, rev_ep, rev_e_pos = rev_result[0], rev_result[1], rev_result[2]
-
-    //convert rev_e_pos in forward search to s_pos in backward search
-    s_pos = read_len - 1 - rev_e_pos
-    e_pos = p
-    if rev_ep - rev_sp + 1 <= MAXIMUM_MATCH {
-        match_pos := make([]int, 0, MAXIMUM_MATCH)
-        for p := rev_sp; p <= rev_ep; p++ {
-            match_pos = append(match_pos, len(I.SEQ) - 1 - I.REV_FMI.SA[p] - (s_pos - e_pos))
-        }
-        return s_pos, e_pos, match_pos, true
-    }
-    return -1, -1, []int{}, false
+    rev_s_pos = READ_LEN - 1 - p
+    rev_sp, rev_ep, rev_e_pos = I.BackwardSearchFrom(I.REV_FMI, rev_read, rev_s_pos)
+	if rev_e_pos >= 0 {
+		var idx int
+		//convert rev_e_pos in forward search to s_pos in backward search
+		s_pos = READ_LEN - 1 - rev_e_pos
+		e_pos = p
+		if rev_ep - rev_sp + 1 <= MAXIMUM_MATCH {
+		    for idx = rev_sp ; idx <= rev_ep ; idx++ {
+		        match_pos[idx - rev_sp] = len(I.SEQ) - 1 - I.REV_FMI.SA[idx] - (s_pos - e_pos)
+		    }
+		    return s_pos, e_pos, rev_ep - rev_sp + 1, true
+		}
+	    return s_pos, e_pos, rev_ep - rev_sp + 1, false
+	}
+    return -1, -1, -1, false // will be changed later
 }
 
 //-----------------------------------------------------------------------------------------------------
 // FindExtension function returns alignment (snp report) between between reads and multi-genomes.
 // The alignment is built within a given threshold of distance.
 //-----------------------------------------------------------------------------------------------------
-func (I Index) FindExtensions(read []byte, s_pos, e_pos int, match_pos int) (int, []byte, []byte,
-	 []byte, []byte, map[int][]byte, map[int][]byte, bool) {
+func (I *Index) FindExtensions(read []byte, s_pos, e_pos int, match_pos int, bw_snp_idx, fw_snp_idx []int, bw_snp_val, fw_snp_val [][]byte, bw_D, fw_D [][]int, bw_T, fw_T [][][]byte) (int, int, int, bool) {
 
-    var ref_left_flank, ref_right_flank []byte
-    read_left_flank := read[ : e_pos]
-    read_right_flank := read[s_pos + 1 : ]
+    var ref_left_flank, ref_right_flank, read_left_flank, read_right_flank []byte
     var lcs_len int = s_pos - e_pos + 1
 
     var isSNP, isSameLenSNP bool
@@ -162,20 +152,21 @@ func (I Index) FindExtensions(read []byte, s_pos, e_pos int, match_pos int) (int
         ref_right_flank = I.SEQ[match_pos + lcs_len : len(I.SEQ)]
     }
 
-    left_d, left_D, left_m, left_n, left_A, left_T, _ :=
-     I.BackwardDistanceMulti(read_left_flank, ref_left_flank, left_most_pos)
-    right_d, right_D, right_m, right_n, right_A, right_T, _ :=
-     I.ForwardDistanceMulti(read_right_flank, ref_right_flank, match_pos + lcs_len)
+    read_left_flank = read[ : e_pos]
+    left_d, left_D, left_m, left_n, left_sn, _ :=
+     I.BackwardDistance(read_left_flank, ref_left_flank, left_most_pos, bw_snp_idx, bw_snp_val, bw_D, bw_T)
+
+    read_right_flank = read[s_pos + 1 : ]
+    right_d, right_D, right_m, right_n, right_sn, _ :=
+     I.ForwardDistance(read_right_flank, ref_right_flank, match_pos + lcs_len, fw_snp_idx, fw_snp_val, fw_D, fw_T)
 
     dis := left_d + right_d + left_D + right_D
     if dis <= DIST_THRES {
-        left_snp := I.BackwardTraceBack(read_left_flank, ref_left_flank,
-         left_m, left_n, left_A, left_T, left_most_pos)
-        right_snp := I.ForwardTraceBack(read_right_flank, ref_right_flank,
-         right_m, right_n, right_A, right_T, match_pos + lcs_len)
-        return dis, read_left_flank, read_right_flank, ref_left_flank, ref_right_flank,
-		left_snp, right_snp, true
+        left_num := I.BackwardTraceBack(read_left_flank, ref_left_flank,
+         left_m, left_n, left_most_pos, left_sn, bw_snp_idx, bw_snp_val, bw_T)
+        right_num := I.ForwardTraceBack(read_right_flank, ref_right_flank,
+         right_m, right_n, match_pos + lcs_len, right_sn, fw_snp_idx, fw_snp_val, fw_T)
+        return dis, left_num, right_num, true
     }
-    return dis, read_left_flank, read_right_flank, ref_left_flank, ref_right_flank,
-	map[int][]byte{}, map[int][]byte{}, false
+    return dis, 0, 0, false
 }
